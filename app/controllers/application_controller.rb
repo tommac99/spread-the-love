ffbd5495de4bec629b606a465a12906eff4455f8
Julia Mackamul <julia.mackamul@gmail.com>
2020-04-03 17:02:25 +0200

    added exception for denied authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
-  
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   after_action :verify_authorized, except: :index
@@ -22,4 +22,12 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    redirect_to(request.referrer || root_path)
+  end
 end
+
+
