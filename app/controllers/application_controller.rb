e521f4d21dcd62819ee1d9876fcb69343b9857bf
Julia Mackamul <julia.mackamul@gmail.com>
2020-04-03 16:59:17 +0200

    added a rescue for denied authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
+  
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
@@ -11,5 +14,12 @@ class ApplicationController < ActionController::Base
     else
       skip_authorization
     end
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
