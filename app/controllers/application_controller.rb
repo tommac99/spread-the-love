04a7f6e52699d7ad5b7493d9dba946b822594628
Julia Mackamul <julia.mackamul@gmail.com>
2020-04-03 16:47:14 +0200

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  def show
+    record = Record.find_by(attribute: "value")
+    if record.present?
+      authorize record
+    else
+      skip_authorization
+    end
+  end
 end
