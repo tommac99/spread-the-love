8388afde8e4aa89982859da9f8b6b818b8a7f9a1
Tom Macfarlaine <tommacfarlaine@Toms-MacBook-Pro.local>
2020-04-04 09:17:29 +0200

    New Develop Branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,53 +1,52 @@
 class PostsController < ApplicationController
  def home
-  @posts = Post.all
-end
+    @posts = Post.all
+  end
 
-def index
-end
+  def index
+  end
 
-def new
-  @post = Post.new
-end
+  def new
+    @post = Post.new
+  end
 
-def show
-  @post = Post.find(params[:id])
-end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-def create
-  @post = Post.new(post_param)
-  @post.user_id = current_user.id
+  def create
+    @post = Post.new(post_param)
+    @post.user_id = current_user.id
 
-  if @post.save!
+    if @post.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_param)
+      redirect_to post_path(@post.id)
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to root_path
-  else
-    render :new
   end
 
-end
-
-def edit
-  @post = Post.find(params[:id])
-end
-
-def update
-  @post = Post.find(params[:id])
-
-  if @post.update(post_param)
-    redirect_to post_path(@post.id)
-  else
-   render :edit
+  def post_param
+    params.require(:post).permit(:title, :description, :category,:location, :group,:date)
   end
 end
-
-def destroy
-  @post = Post.find(params[:id])
-  @post.destroy
-  redirect_to root_path
-end
-
-def post_param
-  params.require(:post).permit(:title, :description, :category,:location, :group,:date)
-end
-end
-
