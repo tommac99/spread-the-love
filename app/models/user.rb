050402030e65d191fbd7c7fd875d4692dacd405c
Julia Mackamul <julia.mackamul@gmail.com>
2020-04-06 22:00:15 +0200

    added regular expression for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  EMAIL_REGEX =  /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
   validates :email, :presence => true,
                     :length: { :maximum => 100},
                     :uniqueness => true,
