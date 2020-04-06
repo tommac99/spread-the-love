0b63d5c11a1d1d1e6c9262c06b79ae32c448bd33
Julia Mackamul <julia.mackamul@gmail.com>
2020-04-06 20:18:16 +0200

    added validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :email, :presence => true.
+                    :length: { :maximum => 100}
+                    :uniqueness => true,
+                    :format => EMAIL_REGEX,
+                    :confirmation => true
+
 end
