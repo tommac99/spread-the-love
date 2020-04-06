5ab3420c731e08a61e3a01f01e5b5482b505abbe
Julia Mackamul <julia.mackamul@gmail.com>
2020-04-06 20:19:43 +0200

    added validation for first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :email, :presence => true.
                     :length: { :maximum => 100}
                     :uniqueness => true,
                     :format => EMAIL_REGEX,
                     :confirmation => true
+
+  validates :first_name, :presence => true.
+                    :length: { :maximum => 25}
+  
 
 end
